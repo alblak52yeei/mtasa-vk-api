421661e7eb2dde045d585bd8a8e03824de7197f9
rdror <84703721+rdror-ru@users.noreply.github.com>
2021-05-24 16:51:13 +0800

    Update VKMessages.lua

--- VKMessages.lua
+++ VKMessages.lua
@@ -3,17 +3,15 @@ local ACCESS_TOKEN = 'your_access_token' -- here is the access_token of the VK g
 local bDebugState = false -- to enable debug mode, set the value to true
 
 local char_to_hex = function(c)
-  return string.format("%%%02X", string.byte(c))
+  	return string.format("%%%02X", string.byte(c))
 end
 
 local function urlencode(url)
-  if url == nil then
-    return
-  end
-  url = url:gsub("\n", "\r\n")
-  url = url:gsub("([^%w ])", char_to_hex)
-  url = url:gsub(" ", "+")
-  return url
+	if url == nil then return end
+	url = url:gsub("\n", "\r\n")
+	url = url:gsub("([^%w ])", char_to_hex)
+	url = url:gsub(" ", "+")
+	return url
 end
 
 function SendMessage( iChat, sMessage )
